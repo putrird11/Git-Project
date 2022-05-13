2277c13a5a4216fc519b3e6a48d5dd1d7ef711a4
putrird11 <putrirahmadewi77@gmail.com>
2022-05-13 23:20:04 +0700

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by PUTRI RAHMADEWI on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by PUTRI RAHMADEWI on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
